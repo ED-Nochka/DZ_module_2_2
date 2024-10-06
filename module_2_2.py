66692c46a845935628bd9a83594798efeac4354e
ED-Nochka <nocevnayaed@gmail.com>
2024-10-06 18:51:37 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,16 @@
+first = 22
+second = 22
+thirs = 22
+if first == second and second == thirs:
+    print('3')
+first = 22
+second = 123
+thirs = 5
+if first == second or second == thirs:
+    print('n')
+else: print('0')
+first = 22
+second = 50
+thirs = 22
+if first == second or first == thirs:
+    print('2')
